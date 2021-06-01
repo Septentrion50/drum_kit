2cd2fc67fbde516f330c5873fe6cf3f069fd4cb0
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-01 21:12:55 +0200

    Ajout du drum kit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'drum#index'
+  resources 'drum', only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
